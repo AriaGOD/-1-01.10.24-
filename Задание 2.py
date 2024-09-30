e2c686de986cbd98db6e868bb9ffc331bc5a6e82
Kirill Osipov <144159424+AriaGOD@users.noreply.github.com>
2024-09-30 17:20:58 +0500

    Add files via upload

---
+++ Задание 2.py
@@ -0,0 +1,5 @@
+number = str(input())
+if int (number [0]) + int (number [1]) + int (number [2]) == int (number [3]) + int (number [4]) + int (number [5]):
+    print ( "Счастливый билет" )
+else:
+    print ( "Несчастливый билет" )
